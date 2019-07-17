88a6fe18a3dda49e71238ef53ce69737e06cd693
Derpific <derpificcodes@gmail.com>
2019-07-17 10:47:47 -0400

    oof

--- src/core/CoreListener.php
+++ src/core/CoreListener.php
@@ -1133,7 +1133,7 @@ class CoreListener implements Listener {
 			$players[] = $onlinePlayer;
 		}
 		foreach($this->core->getNetwork()->getServers() as $server) {
-			//$server->query();
+			$server->query();
 		}
 		$event->setPlayerList($players);
 	}
